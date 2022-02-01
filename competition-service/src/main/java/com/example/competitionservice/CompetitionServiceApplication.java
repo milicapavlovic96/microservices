3f6ab4c56a0170014c463f241c733f703c3a79a9
Milica <pavlovic380@gmail.com>
2022-02-01 21:57:27 +0100

    initial commit

---
+++ competition-service/src/main/java/com/example/competitionservice/CompetitionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.competitionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetitionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetitionServiceApplication.class, args);
+	}
+
+}
---
+++ nacionality-service/src/test/java/com/example/nacionalityservice/NacionalityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nacionalityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacionalityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
