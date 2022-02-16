e8cfb5c0b6d29fcee61bfe54561be94f671286b1
milicapavlovic96 <pavlovic380@gmail.com>
2022-02-16 16:44:30 +0100

    final changes

--- player-service/src/main/resources/data.sql
+++ player-service/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO Player VALUES (100, 'Aleksandar', 'Stojic', 100);
-INSERT INTO Player VALUES (101, 'Nikola', 'Savic', 100);
-INSERT INTO Player VALUES (102, 'Ivan', 'Jura', 100);
+INSERT INTO Player VALUES (100, 'Djordjevic', 'Zoran', 100);
+INSERT INTO Player VALUES (101, 'Majkic', 'Miodrag', 100);
+INSERT INTO Player VALUES (102, 'Todorovic', 'Nikola', 100);
